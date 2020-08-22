8edb73d1780b02b26cd48e1606ac7dc178f51791
Luis <luislucatero21@gmail.com>
2020-08-22 09:53:43 -0500

    Initial Commit

---
+++ ExpandableCellExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpandableCellExample
+//
+//  Created by Luis Ángel Lucatero Villanueva on 22/08/20.
+//  Copyright © 2020 Luis Lucatero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
